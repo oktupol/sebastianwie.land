eba54d34889f9036930e18a87f387359f7b975e6
Sebastian Wieland <git-commits@nwie.land>
2021-11-26 23:35:03 +0100

    wrapper component test

--- src/app/components/title-wrapper/title-wrapper.component.spec.ts
+++ src/app/components/title-wrapper/title-wrapper.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 
 import { TitleWrapperComponent } from './title-wrapper.component';
 
+const mockStore = {
+  select() {
+    return of('up');
+  }
+}
 describe('TitleWrapperComponent', () => {
   let component: TitleWrapperComponent;
   let fixture: ComponentFixture<TitleWrapperComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TitleWrapperComponent ]
+      declarations: [ TitleWrapperComponent ],
+      providers: [
+        { provide: Store, useValue: mockStore }
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +31,11 @@ describe('TitleWrapperComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('position', () => {
+    it('should be set', () => {
+      expect(component.position).toEqual('up');
+    });
   });
 });
