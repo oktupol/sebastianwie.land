9d2312b61559fcf3580e9c2b6525f25fab1b986b
Sebastian Wieland <git-commits@nwie.land>
2021-12-04 13:34:24 +0100

    fixed environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,17 +6,17 @@ export const environment = {
   contact: {
     // Obfuscating information for scrapers on Github
     from: `Contact Form <${[
+      110, 111, 45, 114, 101, 112, 108,
+      121, 64, 115, 101, 98, 97, 115,
+      116, 105, 97, 110, 119, 105, 101,
+      108, 97, 110, 100, 46, 100, 101
+    ].map(b => String.fromCharCode(b)).join('')}>`,
+    to: `Sebastian Wieland <${[
       115, 101, 98, 97, 115, 116, 105,
       43, 99, 111, 110, 116, 97, 99,
       116, 102, 111, 114, 109, 64, 110,
       119, 105, 101, 46, 108, 97, 110,
       100
-    ].map(b => String.fromCharCode(b)).join('')}>`,
-    to: `Sebastian Wieland <${[
-      115, 101, 98, 97, 115,
-      116, 105, 64, 110, 119,
-      105, 101, 46, 108, 97,
-      110, 100
     ].map(b => String.fromCharCode(b)).join('')}>`,
     messageIdDomain: 'sebastianwieland.de',
   }
