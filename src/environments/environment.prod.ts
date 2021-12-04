639982e0741961fd7c634d65b87820823ff7a15d
Sebastian Wieland <git-commits@nwie.land>
2021-12-04 13:47:07 +0100

    apparently I can hide the sender, too

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,12 +5,6 @@ export const environment = {
 
   contact: {
     // Obfuscating information for scrapers on Github
-    from: `Contact Form <${[
-      110, 111, 45, 114, 101, 112, 108,
-      121, 64, 115, 101, 98, 97, 115,
-      116, 105, 97, 110, 119, 105, 101,
-      108, 97, 110, 100, 46, 100, 101
-    ].map(b => String.fromCharCode(b)).join('')}>`,
     to: `Sebastian Wieland <${[
       115, 101, 98, 97, 115, 116, 105,
       43, 99, 111, 110, 116, 97, 99,
