20fa1f010db91c1200a8f300a78aef5b36607412
Sebastian Wieland <git-commits@nwie.land>
2021-12-16 19:38:52 +0100

    contact component test

--- src/app/modules/contact/components/contact/contact.component.spec.ts
+++ src/app/modules/contact/components/contact/contact.component.spec.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { DebugElement, Injectable } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { FormArray, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
 import { EMPTY, Observable, of } from 'rxjs';
-import { Message } from '../interfaces/message';
-import { OpenpgpService } from '../services/openpgp.service';
-import { getContactFormInputs } from '../store/selectors/contact-form.selectors';
+import { Message } from '../../interfaces/message';
+import { ContactFormService } from '../../services/contact-form.service';
+import { getContactFormInputs } from '../../store/selectors/contact-form.selectors';
 
 import { ContactComponent } from './contact.component';
 
@@ -26,36 +27,92 @@ class MockStore {
   dispatch() {
   }
 }
+
 @Injectable()
-class MockOpenPgpService {
-  encryptMessage(message: Message): Observable<Message> {
-    return of({ ...message, message: '3ncryp73d m355463'});
+class MockContactFormService {
+  send() {
+
   }
 }
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
+  let el: DebugElement;
+  let contactFormService: ContactFormService;
+  let store: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactComponent ],
-      imports: [ ReactiveFormsModule ],
+      declarations: [ContactComponent],
+      imports: [ReactiveFormsModule],
       providers: [
         FormBuilder,
         { provide: Store, useClass: MockStore },
-        { provide: OpenpgpService, useClass: MockOpenPgpService },
+        { provide: ContactFormService, useClass: MockContactFormService },
       ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ContactComponent);
+    el = fixture.debugElement;
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    store = TestBed.inject(Store);
+    contactFormService = TestBed.inject(ContactFormService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('attachments', () => {
+    let attachments: FormArray;
+
+    beforeEach(() => {
+      attachments = component.contactForm.get('attachments') as FormArray;
+    });
+
+    describe('addAttachmentField', () => {
+      it('should increase number of attachments', () => {
+        expect(attachments.length).toBe(1);
+
+        component.addAttachmentField();
+
+        expect(attachments.length).toBe(2);
+      });
+    });
+
+    describe('deleteAttachment', () => {
+      it('should reduce number of attachments', () => {
+        component.addAttachmentField();
+        component.addAttachmentField();
+
+        expect(attachments.length).toBe(3);
+
+        component.deleteAttachment(0);
+
+        expect(attachments.length).toBe(2);
+      });
+    });
+
+    describe('reset', () => {
+      it('should reset attachment array', () => {
+        component.addAttachmentField();
+        component.addAttachmentField();
+        component.addAttachmentField();
+
+        expect(attachments.length).toBe(4);
+
+        component.reset();
+
+        expect(component.contactForm.get('attachments')).not.toBe(attachments);
+
+        expect((component.contactForm.get('attachments') as FormArray).length).toBe(1);
+      });
+    });
+  });
+
 });
