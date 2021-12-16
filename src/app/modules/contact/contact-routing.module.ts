9cf34de1400c82b80f87f5903f09043aeaf38313
Sebastian Wieland <git-commits@nwie.land>
2021-12-16 14:08:13 +0100

    starting verify-mail component

--- src/app/modules/contact/contact-routing.module.ts
+++ src/app/modules/contact/contact-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
+import { VerifyMailComponent } from './components/verify-mail/verify-mail.component';
 
-const routes: Routes = [{ path: '', component: ContactComponent }];
+const routes: Routes = [
+  { path: '', component: ContactComponent },
+  { path: 'verify', component: VerifyMailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
