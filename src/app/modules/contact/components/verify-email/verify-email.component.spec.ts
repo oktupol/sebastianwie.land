c442613e38ea4fd717d6d8c80a97313d6706c317
Sebastian Wieland <git-commits@nwie.land>
2021-12-27 11:55:19 +0100

    minor adjustments

--- src/app/modules/contact/components/verify-email/verify-email.component.spec.ts
+++ src/app/modules/contact/components/verify-email/verify-email.component.spec.ts
@@ -1,14 +1,27 @@
+import { Injectable } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable, of } from 'rxjs';
+import { VerificationResponse } from 'src/app/util/types';
+import { VerificationService } from '../../services/verification.service';
 
 import { VerifyEmailComponent } from './verify-email.component';
 
+@Injectable()
+class MockVerificationService {
+  public verify(): Observable<VerificationResponse> {
+    return of('good-signature');
+  }
+}
 describe('VerifyEmailComponent', () => {
   let component: VerifyEmailComponent;
   let fixture: ComponentFixture<VerifyEmailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ VerifyEmailComponent ]
+      declarations: [ VerifyEmailComponent ],
+      providers: [
+        { provide: VerificationService, useClass: MockVerificationService },
+      ]
     })
     .compileComponents();
   });
