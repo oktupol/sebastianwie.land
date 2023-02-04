4393d1f618cd393e4ad6fc06a6c446411498be3e
Sebastian Wieland <git-commits@nwie.land>
2023-02-04 14:47:05 +0100

    update backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,7 @@ export const environment = {
   production: true,
 
   // Obfuscating information for automatic scrapers on Github
-  backendUrl: [
-    104, 116, 116, 112, 115,  58,  47,  47,
-     97, 112, 105,  46,  97, 119, 115,  46,
-    115, 101,  98,  97, 115, 116, 105,  97,
-    110, 119, 105, 101,  46, 108,  97, 110,
-    100
-  ].map(b => String.fromCharCode(b)).join(''),
+  backendUrl: 'https://api.sebastianwie.land',
 
   contact: {
     to: `Sebastian Wieland <${[
